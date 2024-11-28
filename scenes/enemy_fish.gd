37eb47b96f311ef11c5072d8b81f201e22d50b6e
Alisher Battalov <71417582+abattalov@users.noreply.github.com>
2024-11-28 15:05:42 -0600

    added enemy spawning logic
    enemies now spawn off screen and delete when they reach the opposite side

--- scenes/enemy_fish.gd
+++ scenes/enemy_fish.gd
@@ -2,23 +2,38 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const FRICTION = 300.0
-var is_deleted = false;
+var is_deleted = false
+var direction
+var face_direction = 1
 
 @onready var enemy_fish: Sprite2D = %EnemyFish
-	
-func _physics_process(delta):
+
+func _ready():
 	var screen_size = get_viewport().get_visible_rect().size
+	if randf() < 0.5:
+		print("moving right")
+		position = Vector2(-10.0, randf() * screen_size.y)
+		face_direction = 1
+	else:
+		print("moving left")
+		position = Vector2(screen_size.x + 10, randf() * screen_size.y)
+		face_direction = -1
+	
+
+func _physics_process(delta):
+	var screen_size = get_viewport().get_visible_rect().size	
 	var direction = Vector2(1.0,0.0)
+	print(position)
 	
 	if !is_deleted:
-		if position.x > screen_size.x:
+		if face_direction == 1 and position.x > screen_size.x:
 			is_deleted = true
 			queue_free()
-		elif position.x < 0:
+		elif face_direction == -1 and position.x < 0:
 			is_deleted = true
 			queue_free()
 	
-	velocity.x = direction.x * SPEED
+	velocity.x = direction.x * SPEED * face_direction
 	velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 		
 	move_and_slide()
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -10,7 +10,7 @@ const GRAVITY = 20.0
 func _physics_process(delta):
 	
 	var screen_size = get_viewport().get_visible_rect().size #1152,648
-	print(position)
+	#print(position)
 	
 	#wrap around effect
 	if position.x > screen_size.x:
